4a98d8a54f634b0aed9153ef47af8ed014a08fae
JahnaviEluri1011 <115784249+JahnaviEluri1011@users.noreply.github.com>
2023-08-28 19:10:13 +0530

    user_registration_db.py

--- user_registration_db.py
+++ user_registration_db.py
@@ -6,14 +6,14 @@ user_id = sys.argv[1]
 user_name = sys.argv[2]
 user_dob= sys.argv[3]
 user_gender=sys.argv[4]
-user_status=sys.argv[5]
+
 
 # Connect to the SQLite database
 conn = sqlite3.connect('financial_services.db')
 cursor = conn.cursor()
 
 # Create a table if it doesn't exist
-cursor.execute('CREATE TABLE IF NOT EXISTS loan_applications (ID INTEGER PRIMARY KEY,Name TEXT,DOB DATE,Gender TEXT,Status TEXT)')
+cursor.execute('CREATE TABLE IF NOT EXISTS loan_applications (ID INTEGER PRIMARY KEY,Name TEXT,DOB DATE,Gender TEXT)')
 
 
 # Check if user already exists in the database
@@ -22,7 +22,7 @@ user_count = cursor.fetchone()[0]
 
 if user_count == 0:
     # User doesn't exist, insert new user details into the database
-    cursor.execute('INSERT INTO loan_applications (ID,Name, DOB, Gender, Status) VALUES(?,?,?,?,?)',(user_id,user_name,user_dob,user_gender,user_status))
+    cursor.execute('INSERT INTO loan_applications (ID,Name, DOB, Gender, Status) VALUES(?,?,?,?)',(user_id,user_name,user_dob,user_gender))
     conn.commit()
     response = "User registered successfully."
 else:
